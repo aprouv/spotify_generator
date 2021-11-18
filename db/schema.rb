983f27a0dde52c966f5c2d6b8983b83ef8212850
Andrea Prouvost <andrea.prouvost@gmail.com>
2021-11-18 11:41:02 +0100

    add spotify_id to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_111813) do
+ActiveRecord::Schema.define(version: 2021_11_17_103857) do
 
   create_table "playlists", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_111813) do
     t.integer "energy"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "spotify_id"
   end
 
 end
