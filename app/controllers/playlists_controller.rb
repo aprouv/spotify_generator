0d09f5b891ed32990ced4bdd87b333681b77e66b
Andrea Prouvost <andrea.prouvost@gmail.com>
2021-11-22 16:04:03 +0100

    modify create method in playlists_controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -18,7 +18,7 @@ class PlaylistsController < ApplicationController
     @playlist = Playlist.new(playlist_params)
 
     if @playlist.valid?
-      PlaylistCreator.new(@playlist).generate_from_seeds
+      @playlist.spotify_id = PlaylistCreator.new(@playlist).generate_from_seeds
     end
 
     if @playlist.save
