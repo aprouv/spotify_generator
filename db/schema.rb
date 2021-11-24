a49ecceab2181c5a6c99489c33a225a990fff229
Andrea Prouvost <andrea.prouvost@gmail.com>
2021-11-24 22:21:00 +0100

    Add user_id to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_171435) do
+ActiveRecord::Schema.define(version: 2021_11_23_161755) do
 
   create_table "playlists", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_11_22_171435) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "spotify_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_11_22_171435) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "playlists", "users"
 end
