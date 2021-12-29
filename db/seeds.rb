65b3275c15dac8121d160b09c2a6b5b20253627a
Andrea Prouvost <andrea.prouvost@gmail.com>
2021-12-29 16:43:45 +0100

    modify seed with user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 
 puts "Creating playlists..."
 
-User.create(email: "test@test.com", password: "coucou")
+user = User.create(email: "test@test.com", password: "coucou")
 
-Playlist.create(name: "Playlist1", energy: "60", danceability: "65", genre: "rock", user_id: 1)
-Playlist.create(name: "Playlist2", energy: "25", danceability: "30", user_id: 1)
-Playlist.create(name: "Playlist3", energy: "45", danceability: "40", user_id: 1)
+user.playlists.create(name: "Playlist1", energy: "60", danceability: "65", genre: "rock")
+user.playlists.create(name: "Playlist2", energy: "25", danceability: "30")
+user.playlists.create(name: "Playlist3", energy: "45", danceability: "40")
 
 puts "Done !"
