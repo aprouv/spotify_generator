17ebd61c4bed8a86787d66d881a9ed132206ff9a
Andréa Prouvost <77682753+aprouv@users.noreply.github.com>
2021-12-31 10:48:08 +0100

    replace user_id by user (validations)
    Co-authored-by: Delmaire Loïc <skelz0r@gmail.com>

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,6 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-  validates :user_id, presence: true
+  validates :user, presence: true
   validates :name, presence: true
   validate :at_least_one_critera_is_filled
   validates :energy, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100, only_integer: true, allow_nil: true }
