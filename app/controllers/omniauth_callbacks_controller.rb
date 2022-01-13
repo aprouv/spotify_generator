c3dc5ea54f6f87109e4e25d38f5291eccdcdc44e
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-01-13 12:08:23 +0100

    refactor omniauth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,15 +1,11 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def spotify
-    p request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    @user.refresh_token = request.env["omniauth.auth"].credentials.refresh_token
-    @user.token = request.env["omniauth.auth"].credentials["token"]
 
     if @user.persisted?
+      affect_spotify_tokens!(request.env["omniauth.auth"].credentials)
       sign_in_and_redirect @user # this will throw if @user is not activated
-      current_user = @user
-      current_user.save
       set_flash_message(:notice, :success, kind: "Spotify") if is_navigational_format?
     else
       session["devise.spotify_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
@@ -20,5 +16,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     flash[:error] = "La connexion a échoué. Merci de réessayer ultérieurement."
     redirect_to root_path
+  end
+
+  private
+
+  def affect_spotify_tokens!(credentials)
+    @user.refresh_token = credentials.refresh_token
+    @user.token = credentials.token
+    current_user = @user
+    current_user.save
   end
 end
