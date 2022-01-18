4a49a7fef53ee80809a537f84255035611831655
Andrea Prouvost <andrea.prouvost@gmail.com>
2022-01-18 13:02:53 +0100

    refactor services

--- app/services/playlist_destructor.rb
+++ app/services/playlist_destructor.rb
@@ -1,31 +1,7 @@
-class PlaylistDestructor
-
-  def initialize(playlist)
-    @playlist = playlist
-  end
-
+class PlaylistDestructor < AbstractPlaylistProvider
   def delete_playlist
-    user = retrieve_user
+    user = retrieve_provider_user
     playlist = RSpotify::Playlist.find(@playlist.user.uid, @playlist.spotify_id)
     user.unfollow(playlist)
-  end
-
-  private
-
-  def retrieve_user
-    authenticate_on_spotify
-    RSpotify::User.new(
-      'id' => @playlist.user.uid,
-      'credentials' => {
-        'refresh_token' => @playlist.user.refresh_token,
-        'token' => @playlist.user.token,
-        }
-    )
-  end
-
-  def authenticate_on_spotify
-    spotify_id = ENV["CLIENT_ID"]
-    spotify_secret = ENV["CLIENT_SECRET"]
-    RSpotify::authenticate(spotify_id, spotify_secret)
   end
 end
